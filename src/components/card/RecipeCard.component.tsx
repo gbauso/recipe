b4efc8b4a635e5421c67bde7b3a2600ae67021ab
Gbauso <guilherme.bauso@gmail.com>
2021-03-14 15:23:58 +0100

    feat: recipe card

---
+++ src/components/card/RecipeCard.component.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import Card from './Card.component';
+
+type RecipeCardProps = {
+  name: string;
+  id: string;
+  onClick: (id: string) => void;
+};
+
+const Wrapper = styled(Card)`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin-bottom: 6px;
+`;
+
+const Name = styled.span`
+  margin-left: 16px;
+  color: ${({ theme }) => theme.color.deepBlack};
+`;
+
+const RecipeCard: React.FC<RecipeCardProps> = ({ name, id, onClick }) => {
+  const handleClick = useCallback(
+    (event: React.MouseEvent) => {
+      event.preventDefault();
+      onClick(id);
+    },
+    [onClick, id]
+  );
+
+  return (
+    <Wrapper onClick={handleClick}>
+      <Name>{name}</Name>
+    </Wrapper>
+  );
+};
+
+export default RecipeCard;
