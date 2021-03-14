6e1ad7630d716ee5be040244a858f1bd85559a62
Gbauso <guilherme.bauso@gmail.com>
2021-03-14 16:10:20 +0100

    feat: add wrapper to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import ActionBar from '@/components/action-bar/ActionBar.component';
 import RecipeList from '@/components/recipe-list/RecipeList.component';
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
 
 const Home: React.FC = () => {
   return (
-    <>
+    <Wrapper>
       <RecipeList />
       <ActionBar />
-    </>
+    </Wrapper>
   );
 };
 
