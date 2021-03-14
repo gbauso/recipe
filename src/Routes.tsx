2531a0bf27c1166352464af6e3554d41289c5f71
Gbauso <guilherme.bauso@gmail.com>
2021-03-14 15:43:17 +0100

    fix: form on routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout.component';
+import RecipeForm from './components/recipe-form/RecipeForm.component';
 import RecipeViewer from './components/recipe-viewer/RecipeViewer.component';
 import Home from './pages/Home';
 
@@ -11,6 +12,7 @@ const Routes: FC = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/recipe/:id" component={RecipeViewer} />
+          <Route exact path="/create" component={RecipeForm} />
         </Switch>
       </Layout>
     </Router>
