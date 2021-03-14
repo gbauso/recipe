ba002eefb4f1896d703efa3987e3987e2c827e87
Gbauso <guilherme.bauso@gmail.com>
2021-03-14 14:43:19 +0100

    feat: recipe model

---
+++ src/models/recipe.ts
@@ -0,0 +1,17 @@
+type Recipe = {
+  id: string;
+  dateAdded?: Date;
+  name: string;
+  ingredients: Ingredient[];
+  steps: Step[];
+};
+
+export type Ingredient = {
+  name: string;
+};
+
+export type Step = {
+  description: string;
+};
+
+export default Recipe;
